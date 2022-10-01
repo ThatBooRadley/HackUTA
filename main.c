d3df66071a1131fcb17b842d0d5e022c693a10c7
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2022-10-01 15:44:33 -0500

    updated main.c, removed turing.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
+
+struct map
 {
-    
+    char* key;
+    char* value;
+};
+
+int main(int argc, char** argv)
+{
+    FILE* fin = fopen(argv[1], "r");
+    int defSize = 0;
+    fscanf(fin, "%d", &defSize);
+    struct map* dictionary = calloc(defSize, sizeof(struct map));
+
+    //translate(fin);
+    return 0;
+}
+
+FILE* translate(FILE* fin)
+{
     return 0;
 }
