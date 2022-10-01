d1d21c379c26ad0bd32a90eb96b3897f8e5257ce
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2022-10-01 16:26:17 -0500

    added translate()

--- main.c
+++ main.c
@@ -1,23 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 struct map
 {
     char* key;
-    char* value;
+    char** value;
 };
+
+FILE* translate(FILE* din, struct map* dict);
+void convert(FILE* fin, struct map* dict);
 
 int main(int argc, char** argv)
 {
-    FILE* fin = fopen(argv[1], "r");
+    FILE* din = fopen(argv[1], "r");
     int defSize = 0;
-    fscanf(fin, "%d", &defSize);
+    fscanf(din, "%d", &defSize);
     struct map* dictionary = calloc(defSize, sizeof(struct map));
 
-    //translate(fin);
+    dictionary = translate(din, dictionary);
+
     return 0;
 }
 
-FILE* translate(FILE* fin)
+struct map* translate(FILE* din, struct map* dict)
 {
-    return 0;
+    int index = 0;
+    while(!feof(din))
+    {
+        char* buffer;
+        fscanf(din, "%s", &buffer);
+        if(strcmp(buffer, "<<"))
+        {
+            fscanf(din, "%s", &dict[index].key);
+        }
+        else if(strcmp(buffer, "::"))
+        {
+            fscanf(din, "%s", buffer);
+            for(int i=0;!strcmp(buffer, ">>");i++)
+            {
+                dict[index].value[i] = buffer;
+                fscanf(din, "%s", buffer);
+            }
+        }
+    }    
+    return dict;
+}
+
+void convert(FILE* fin, struct map* dict)
+{
+    int index = 0;
+    while(!feof(fin))
+    {
+        char* buffer;
+        fscanf(fin, "%s", &buffer);
+
+    }
 }
