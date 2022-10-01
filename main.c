3eebf6963608cabeea7056464c38ad9e5eb3a51b
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2022-10-01 16:41:49 -0500

    added convert()

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ struct map
 };
 
 FILE* translate(FILE* din, struct map* dict);
-void convert(FILE* fin, struct map* dict);
+void convert(FILE* fin, struct map* dict, int length);
 
 int main(int argc, char** argv)
 {
@@ -31,28 +31,39 @@ struct map* translate(FILE* din, struct map* dict)
         fscanf(din, "%s", &buffer);
         if(strcmp(buffer, "<<"))
         {
-            fscanf(din, "%s", &dict[index].key);
+            fscanf(din, "%s", &(dict[index]).key);
         }
         else if(strcmp(buffer, "::"))
         {
-            fscanf(din, "%s", buffer);
-            for(int i=0;!strcmp(buffer, ">>");i++)
+            int i=1;
+            do
             {
-                dict[index].value[i] = buffer;
                 fscanf(din, "%s", buffer);
-            }
+                (dict[index]).value[i] = buffer;
+            }while(strcmp(buffer, ">>"));
+            (dict[index]).value[0] = i;
         }
     }    
     return dict;
 }
 
-void convert(FILE* fin, struct map* dict)
+void convert(FILE* fin, struct map* dict, int length)
 {
+    FILE* fout = fopen("fout.c", "w");
     int index = 0;
     while(!feof(fin))
     {
         char* buffer;
         fscanf(fin, "%s", &buffer);
-
+        for(int i=0; i<length; i++)
+        {
+            if(strcmp(buffer, (dict[i]).key))
+            {
+                for(int j=1; j < (dict[i]).value[0];j++)
+                {
+                    fprintf(fout, "%s", (dict[i]).value[j]);
+                }
+            }
+        }
     }
 }
